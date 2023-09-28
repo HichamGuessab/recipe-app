ecdf63b925252a01dfa7462011a6130c60a835a6
Hicham Guessab <hicham.guessab@cbainfo.fr>
2023-09-28 14:38:25 +0200

    Permettre à l'utilisateur d'ajouter des ingrédients à la shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {Ingredient} from "../../shared/ingedient.model";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent {
-
+  @Output() ingredientAdded: EventEmitter<Ingredient> = new EventEmitter<Ingredient>();
+  addIngredient(name: string, amount: string) {
+    this.ingredientAdded.emit(new Ingredient(name, +amount));
+  }
 }
