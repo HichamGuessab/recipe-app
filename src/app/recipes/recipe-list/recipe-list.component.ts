259823bb7353abc7046b96026b0e07f09df96e86
Hicham Guessab <hicham.guessab@cbainfo.fr>
2023-09-28 11:11:06 +0200

    Affichage des détails d'une Recette avec des Events et des Property Bindings Combinés.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from "../recipe.model";
 
 @Component({
@@ -10,10 +10,14 @@ export class RecipeListComponent implements OnInit{
   recipes: Recipe[] = [
     new Recipe('test', 'description test', 'https://img.freepik.com/photos-premium/1-croissants-isoles-fond-blanc-petit-dejeuner-collations-boulangerie_319982-834.jpg')
   ];
+  @Output() recipeWasSelected: EventEmitter<Recipe> = new EventEmitter<Recipe>();
 
   constructor() {
   }
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
