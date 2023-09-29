c38c8c0b9596bce0a4258b2d4a316135d4b66254
Hicham Guessab <hicham.guessab@cbainfo.fr>
2023-09-29 16:23:07 +0200

    Adding ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,19 @@
 import {Recipe} from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingedient.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('test', 'description test', 'https://img.freepik.com/photos-premium/1-croissants-isoles-fond-blanc-petit-dejeuner-collations-boulangerie_319982-834.jpg')
+    new Recipe(
+      'Croissant',
+      'Un petit croissant.',
+      'https://img.freepik.com/photos-premium/1-croissants-isoles-fond-blanc-petit-dejeuner-collations-boulangerie_319982-834.jpg',
+      [
+        new Ingredient('Beurre', 7),
+        new Ingredient('Encore du beurre', 4)
+      ])
   ];
   getRecipes() {
     return this.recipes.slice();
