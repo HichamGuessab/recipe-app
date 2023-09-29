68ebd064da38c0331239bbb4a711ed3fefa95644
Hicham Guessab <hicham.guessab@cbainfo.fr>
2023-09-29 16:09:03 +0200

    Error : Adding the Shopping List Service

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,14 +1,19 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Ingredient} from "../../shared/ingedient.model";
+import {ShoppingListService} from "../shopping-list.service";
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent {
-  @Output() ingredientAdded: EventEmitter<Ingredient> = new EventEmitter<Ingredient>();
+export class ShoppingEditComponent implements OnInit{
+  constructor(private shoppingListService: ShoppingListService) {
+  }
+  ngOnInit(): void {
+  }
+
   addIngredient(name: string, amount: string) {
-    this.ingredientAdded.emit(new Ingredient(name, +amount));
+    this.shoppingListService.addIngredient(new Ingredient(name, +amount))
   }
 }
